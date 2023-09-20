6fdb5885c0913d2f13765ab0d6d9bf9db01101b2
EnriquedeCastro <143217608+EnriquedeCastro@users.noreply.github.com>
2023-09-20 11:41:52 -0700

    Add files via upload

---
+++ for loops-copy/for_loops.java
@@ -0,0 +1,13 @@
+public class for_loops {
+    public static void main(String[] args) {
+        for (int i = 0; i >= 15; i++) {
+                System.out.println("i is equal to" + i);
+        }
+        for (int j = 10; j == 0; j= j - 1) {
+            System.out.println("j is equal to" + j);
+        }
+        for (int k = 10; k == 0; k= k - 3) {
+            System.out.println("k is equal to" + k);
+        }
+    }
+}
